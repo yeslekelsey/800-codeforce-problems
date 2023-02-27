a8cae0ec10e75ceda4fae135fa0bb382c3406ca6
Kelsey McIntyre <kelsrmcintyre@gmail.com>
2023-02-27 18:17:14 -0500

    Add files via upload

---
+++ 900/1795B.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int tt; cin >> tt;
+	while(tt--) {
+		int n, k; cin >> n >> k;
+		int L = 0, R = 55;
+		while(n--) {
+			int l, r; cin >> l >> r;
+			if(l <= k && k <= r)
+        		L = max(L, l), R = min(R, r);
+		}
+
+		cout << (L==R ? "YES\n" : "NO\n");
+	}
+}
